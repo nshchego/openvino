6af39b5a3ebfbd105537b5efa2184cc86a4fea37
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2026-02-17 17:16:44 +0400

    TBB threading

--- src/tests/functional/base_func_tests/src/base/ov_subgraph.cpp
+++ src/tests/functional/base_func_tests/src/base/ov_subgraph.cpp
@@ -14,6 +14,7 @@
 #endif
 
 #include "openvino/pass/manager.hpp"
+#include "openvino/core/parallel.hpp"
 #include "openvino/core/preprocess/pre_post_process.hpp"
 #include "openvino/pass/serialize.hpp"
 #include "transformations/convert_precision.hpp"
@@ -463,52 +464,44 @@ std::vector<ov::Tensor> SubgraphBaseTest::get_plugin_outputs() {
 }
 
 void SubgraphBaseTest::validate() {
-    std::vector<ov::Tensor> expectedOutputs, actualOutputs;
-    std::exception_ptr expected_outputs_error, actual_output_error;
+    std::vector<ov::Tensor> expected_outputs, actual_outputs;
+    std::exception_ptr inference_exception;
 
 #ifndef NDEBUG
-    actualOutputs = get_plugin_outputs();
-    expectedOutputs = calculate_refs();
+    actual_outputs = get_plugin_outputs();
+    expected_outputs = calculate_refs();
 #else
-    std::thread t_device([this, &actualOutputs, &actual_output_error] {
+    parallel_nt_static(2, [&](const int ithr, const int nthr) {  // TODO: try parallel_nt()
         // The try ... catch block is required to handle exceptions during output calculations and report as test fail.
         // If exception is not caught then application would be terminated with crash. (CVS-133676)
         try {
-            actualOutputs = get_plugin_outputs();
+            if (ithr == 0) {
+                actual_outputs = get_plugin_outputs();
+            } else if (ithr == 1) {
+                expected_outputs = calculate_refs();
+            }
         } catch (...) {
-            actual_output_error = std::current_exception();
+            inference_exception = std::current_exception();
         }
     });
-    std::thread t_ref([this, &expectedOutputs, &expected_outputs_error] {
-        try {
-            expectedOutputs = calculate_refs();
-        } catch (...) {
-            expected_outputs_error = std::current_exception();
-        }
-    });
-    t_device.join();
-    t_ref.join();
 
-    if (actual_output_error) {
-        std::rethrow_exception(actual_output_error);
-    }
-    if (expected_outputs_error) {
-        std::rethrow_exception(expected_outputs_error);
+    if (inference_exception) {
+        std::rethrow_exception(inference_exception);
     }
 #endif
 
-    if (expectedOutputs.empty()) {
+    if (expected_outputs.empty()) {
         return;
     }
 
-    ASSERT_EQ(actualOutputs.size(), expectedOutputs.size())
-        << "TEMPLATE plugin has " << expectedOutputs.size() << " outputs, while " << targetDevice << " " << actualOutputs.size();
+    ASSERT_EQ(actual_outputs.size(), expected_outputs.size())
+        << "TEMPLATE plugin has " << expected_outputs.size() << " outputs, while " << targetDevice << " " << actual_outputs.size();
     if (is_report_stages) {
         std::cout << "[ COMPARATION ] `ov_tensor_utils.hpp::compare()` is started"<< std::endl;
     }
     auto start_time = std::chrono::system_clock::now();
 
-    compare(expectedOutputs, actualOutputs);
+    compare(expected_outputs, actual_outputs);
     if (is_report_stages) {
         auto end_time = std::chrono::system_clock::now();
         std::chrono::duration<double> duration = end_time - start_time;
