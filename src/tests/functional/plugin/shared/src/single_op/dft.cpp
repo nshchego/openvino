9a200493f783945b59c8d6d2e29c4a9a69cf974c
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2026-03-09 12:14:21 +0400

    m_parallel_validation = false

--- src/tests/functional/plugin/shared/src/single_op/dft.cpp
+++ src/tests/functional/plugin/shared/src/single_op/dft.cpp
@@ -93,6 +93,7 @@ std::string DFTLayerTest::getTestCaseName(const testing::TestParamInfo<DFTParams
 }
 
 void DFTLayerTest::SetUp() {
+    m_parallel_validation = true;
     const auto& [shapes, model_type, axes, signal_size, op_type, dev, axes_in_type, size_in_type] = this->GetParam();
     targetDevice = dev;
 
